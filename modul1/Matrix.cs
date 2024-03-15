ed6256912f9e45fbaed9ec75382ec4460cd8d8fe
Danylo <you@example.com>
2024-03-15 10:40:22 +0200

    modul1

---
+++ modul1/Matrix.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul1
+{
+    public class Matrix
+    {
+        public int[][] mat { get; set; }
+        Random random = new Random();
+        public int row { get; set; }
+        public int col { get; set; }
+        public void FillMatrixRandom(int rows, int columns, int min, int max)
+        {
+            mat = new int[rows][];
+            row = rows;
+            col = columns;
+            for (int i = 0; i < rows; i++)
+            {
+                mat[i] = new int[columns];
+            }
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    mat[i][j] = random.Next(min, max);
+                }
+            }
+        }
+        public int this[int row, int column] { get { return mat[row][column]; } set { mat[row][column] = value; } }
+    }
+}
---
+++ modul1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace modul1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var mat = new Matrix();
+            mat.FillMatrixRandom(4, 5, 0, 10);
+            for (int i = 0; i < mat.row; i++)
+            {
+                for (int j = 0; j < mat.col; j++)
+                {
+                    Console.Write(mat[i, j]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            for (int i = 0; i < mat.row; i++)
+            {
+                for (int j = 0; j < mat.col; j++)
+                {
+                    if (mat[i, j] == 5)
+                    {
+                        Console.Write(mat[i, j]);
+                        Console.Write(" ");
+                    }
+                   
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(mat[1, 1]);
+        }
+    }
+}
